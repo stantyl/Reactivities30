3e9e9f11e0763b82d909b36b42ebe5a53508d294
stan tyl <lftparis@outlook.com>
2020-08-02 23:50:10 +0200

    second commit

--- API/Program.cs
+++ API/Program.cs
@@ -10,10 +10,12 @@ using Persistence;
 
 namespace API
 {
+
     public class Program
     {
         public static void Main(string[] args)
         {
+
             var host = CreateHostBuilder(args).Build();
 
             using (var scope = host.Services.CreateScope())
